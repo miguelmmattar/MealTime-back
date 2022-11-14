87ebefde5a1d90755571b8b583c2314323ac76d7
Miguel Faria <mattar1704miguel@gmail.com>
2022-11-14 12:38:05 -0300

    changed nodemon to node on start script

--- dump.sql
+++ dump.sql
@@ -169,28 +169,6 @@ ALTER SEQUENCE public.ingredients_id_seq OWNED BY public.ingredients.id;
 
 
 --
--- Name: ingredients_name_seq; Type: SEQUENCE; Schema: public; Owner: vpusihwbpvbaxr
---
-
-CREATE SEQUENCE public.ingredients_name_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.ingredients_name_seq OWNER TO vpusihwbpvbaxr;
-
---
--- Name: ingredients_name_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: vpusihwbpvbaxr
---
-
-ALTER SEQUENCE public.ingredients_name_seq OWNED BY public.ingredients.name;
-
-
---
 -- Name: recipes; Type: TABLE; Schema: public; Owner: vpusihwbpvbaxr
 --
 
@@ -535,13 +513,6 @@ ALTER TABLE ONLY public.ingredients ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- Name: ingredients name; Type: DEFAULT; Schema: public; Owner: vpusihwbpvbaxr
---
-
-ALTER TABLE ONLY public.ingredients ALTER COLUMN name SET DEFAULT nextval('public.ingredients_name_seq'::regclass);
-
-
---
 -- Name: recipes id; Type: DEFAULT; Schema: public; Owner: vpusihwbpvbaxr
 --
 
@@ -624,15 +595,14 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 COPY public.categories (id, name) FROM stdin;
 1	pasta
-2	meat
-3	chicken
+2	chicken
+3	meat
 4	fish
-5	sea food
-6	soup
-7	pizza
-8	snack
-9	dessert
-10	salad
+5	snack
+6	salad
+7	dessert
+8	sea food
+9	soup
 \.
 
 
@@ -641,7 +611,6 @@ COPY public.categories (id, name) FROM stdin;
 --
 
 COPY public.images (id, url, "recipeId") FROM stdin;
-1	https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1001491_11-2e0fa5c.jpg?quality=90&webp=true&resize=300,272	33
 \.
 
 
@@ -650,14 +619,14 @@ COPY public.images (id, url, "recipeId") FROM stdin;
 --
 
 COPY public.ingredients (id, name) FROM stdin;
-105	pancetta
-106	large eggs
-107	sea salt and freshly ground black pepper
-108	spaghetti
-109	unsalted butter
-110	pecorino cheese
-111	parmesan
-112	plump garlic cloves, peeled and left whole
+1	pancetta
+2	pecorino cheese
+3	unsalted butter
+4	parmesan
+5	spaghetti
+6	large eggs
+7	plump garlic cloves, peeled and left whole
+8	sea salt and freshly ground black pepper
 \.
 
 
@@ -666,7 +635,6 @@ COPY public.ingredients (id, name) FROM stdin;
 --
 
 COPY public.recipes (id, "userId", name, serves, "prepTime", method, "createdAt") FROM stdin;
-33	1	Spaghetti Carbonara	4	20	STEP 1\nPut a large saucepan of water on to boil.\n\nSTEP 2\nFinely chop the 100g pancetta, having first removed any rind. Finely grate 50g pecorino cheese and 50g parmesan and mix them together.\n\nSTEP 3\nBeat the 3 large eggs in a medium bowl and season with a little freshly grated black pepper. Set everything aside.\n\nSTEP 4\nAdd 1 tsp salt to the boiling water, add 350g spaghetti and when the water comes back to the boil, cook at a constant simmer, covered, for 10 minutes or until al dente (just cooked).\n\nSTEP 5\nSquash 2 peeled plump garlic cloves with the blade of a knife, just to bruise it.\n\nSTEP 6\nWhile the spaghetti is cooking, fry the pancetta with the garlic. Drop 50g unsalted butter into a large frying pan or wok and, as soon as the butter has melted, tip in the pancetta and garlic.\n\nSTEP 7\nLeave to cook on a medium heat for about 5 minutes, stirring often, until the pancetta is golden and crisp. The garlic has now imparted its flavour, so take it out with a slotted spoon and discard.\n\nSTEP 8\nKeep the heat under the pancetta on low. When the pasta is ready, lift it from the water with a pasta fork or tongs and put it in the frying pan with the pancetta. Don’t worry if a little water drops in the pan as well (you want this to happen) and don’t throw the pasta water away yet.\n\nSTEP 9\nMix most of the cheese in with the eggs, keeping a small handful back for sprinkling over later.\n\nSTEP 10\nTake the pan of spaghetti and pancetta off the heat. Now quickly pour in the eggs and cheese. Using the tongs or a long fork, lift up the spaghetti so it mixes easily with the egg mixture, which thickens but doesn’t scramble, and everything is coated.\n\nSTEP 11\nAdd extra pasta cooking water to keep it saucy (several tablespoons should do it). You don’t want it wet, just moist. Season with a little salt, if needed.\n\nSTEP 12\nUse a long-pronged fork to twist the pasta on to the serving plate or bowl. Serve immediately with a little sprinkling of the remaining cheese and a grating of black pepper. If the dish does get a little dry before serving, splash in some more hot pasta water and the glossy sauciness will be revived.	2022-11-11 01:13:22.676349
 \.
 
 
@@ -675,7 +643,6 @@ COPY public.recipes (id, "userId", name, serves, "prepTime", method, "createdAt"
 --
 
 COPY public."recipes/categories" (id, "recipeId", "categoryId") FROM stdin;
-20	33	1
 \.
 
 
@@ -684,14 +651,6 @@ COPY public."recipes/categories" (id, "recipeId", "categoryId") FROM stdin;
 --
 
 COPY public."recipes/ingredients" (id, "recipeId", "ingredientId", quantity) FROM stdin;
-97	33	109	50g
-98	33	112	2
-99	33	111	50g
-100	33	108	350g
-101	33	105	100g
-102	33	110	50g
-103	33	106	3
-104	33	107	\N
 \.
 
 
@@ -700,10 +659,6 @@ COPY public."recipes/ingredients" (id, "recipeId", "ingredientId", quantity) FRO
 --
 
 COPY public.sessions (id, "userId", token, "createdAt", "closedAt") FROM stdin;
-16	1	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODAwNTc3MX0.uynXC0Rq1KpFDI5cIFMh924lH7ERpEoUpGondMXtbT0	2022-11-09 14:56:11.594119	2022-11-09 15:20:23.245563
-17	1	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODAwNzUzOX0.CYTZSIYByjlYUNX_b4qOX2EJjE33lbX84fyZNN5EWBU	2022-11-09 15:25:39.668217	2022-11-09 15:25:58.913433
-18	1	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODAwNzU2Nn0.8SIdNPb5ngICelaJ5164yZD2uorfhpP-1l2X2jhDH_I	2022-11-09 15:26:06.305429	2022-11-09 16:43:33.087202
-19	1	eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY2ODAxMjI1Nn0.q-wj2pTkd7ycf4u2CDQMVPt1tXxvs84wCO1ygCA0_9A	2022-11-09 16:44:17.132003	\N
 \.
 
 
@@ -712,8 +667,6 @@ COPY public.sessions (id, "userId", token, "createdAt", "closedAt") FROM stdin;
 --
 
 COPY public.users (id, name, email, password, "createdAt") FROM stdin;
-1	Miguel	miguel@email.com	$2b$10$mlJaGPWi.p3bHeOOwQKM7OKdaFPxxmuV.Gs3xD.dR1t6XsVag9jXG	2022-11-09 14:13:26.343622
-2	Luiza	luiza@email.com	$2b$10$x3KEFMHlHEi3t.MwkY9b9uHrZpr01iTPNyXhs45eVc2BrUsOvTiM2	2022-11-09 14:25:13.140915
 \.
 
 
@@ -721,14 +674,14 @@ COPY public.users (id, name, email, password, "createdAt") FROM stdin;
 -- Name: categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public.categories_id_seq', 11, true);
+SELECT pg_catalog.setval('public.categories_id_seq', 9, true);
 
 
 --
 -- Name: images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public.images_id_seq', 1, true);
+SELECT pg_catalog.setval('public.images_id_seq', 2, true);
 
 
 --
@@ -742,14 +695,7 @@ SELECT pg_catalog.setval('public."images_recipeId_seq"', 1, false);
 -- Name: ingredients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public.ingredients_id_seq', 112, true);
-
-
---
--- Name: ingredients_name_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
---
-
-SELECT pg_catalog.setval('public.ingredients_name_seq', 1, false);
+SELECT pg_catalog.setval('public.ingredients_id_seq', 8, true);
 
 
 --
@@ -763,7 +709,7 @@ SELECT pg_catalog.setval('public."recipes/categories_categoryId_seq"', 1, false)
 -- Name: recipes/categories_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public."recipes/categories_id_seq"', 20, true);
+SELECT pg_catalog.setval('public."recipes/categories_id_seq"', 3, true);
 
 
 --
@@ -777,7 +723,7 @@ SELECT pg_catalog.setval('public."recipes/categories_recipeId_seq"', 1, false);
 -- Name: recipes/ingredients_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public."recipes/ingredients_id_seq"', 104, true);
+SELECT pg_catalog.setval('public."recipes/ingredients_id_seq"', 16, true);
 
 
 --
@@ -798,7 +744,7 @@ SELECT pg_catalog.setval('public."recipes/ingredients_recipeId_seq"', 1, false);
 -- Name: recipes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public.recipes_id_seq', 33, true);
+SELECT pg_catalog.setval('public.recipes_id_seq', 3, true);
 
 
 --
@@ -812,7 +758,7 @@ SELECT pg_catalog.setval('public."recipes_userId_seq"', 1, false);
 -- Name: sessions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: vpusihwbpvbaxr
 --
 
-SELECT pg_catalog.setval('public.sessions_id_seq', 19, true);
+SELECT pg_catalog.setval('public.sessions_id_seq', 1, true);
 
 
 --
@@ -898,7 +844,23 @@ ALTER TABLE ONLY public.users
 --
 
 ALTER TABLE ONLY public.images
-    ADD CONSTRAINT images_fk0 FOREIGN KEY ("recipeId") REFERENCES public.recipes(id);
+    ADD CONSTRAINT images_fk0 FOREIGN KEY ("recipeId") REFERENCES public.recipes(id) ON DELETE CASCADE;
+
+
+--
+-- Name: recipes/ingredients receipe/ingredients_fk0; Type: FK CONSTRAINT; Schema: public; Owner: vpusihwbpvbaxr
+--
+
+ALTER TABLE ONLY public."recipes/ingredients"
+    ADD CONSTRAINT "receipe/ingredients_fk0" FOREIGN KEY ("recipeId") REFERENCES public.recipes(id) ON DELETE CASCADE;
+
+
+--
+-- Name: recipes/ingredients receipe/ingredients_fk1; Type: FK CONSTRAINT; Schema: public; Owner: vpusihwbpvbaxr
+--
+
+ALTER TABLE ONLY public."recipes/ingredients"
+    ADD CONSTRAINT "receipe/ingredients_fk1" FOREIGN KEY ("ingredientId") REFERENCES public.ingredients(id) ON DELETE CASCADE;
 
 
 --
@@ -906,7 +868,7 @@ ALTER TABLE ONLY public.images
 --
 
 ALTER TABLE ONLY public."recipes/categories"
-    ADD CONSTRAINT "recipes/categories_fk0" FOREIGN KEY ("recipeId") REFERENCES public.recipes(id);
+    ADD CONSTRAINT "recipes/categories_fk0" FOREIGN KEY ("recipeId") REFERENCES public.recipes(id) ON DELETE CASCADE;
 
 
 --
@@ -914,23 +876,7 @@ ALTER TABLE ONLY public."recipes/categories"
 --
 
 ALTER TABLE ONLY public."recipes/categories"
-    ADD CONSTRAINT "recipes/categories_fk1" FOREIGN KEY ("categoryId") REFERENCES public.categories(id);
-
-
---
--- Name: recipes/ingredients recipes/ingredients_fk0; Type: FK CONSTRAINT; Schema: public; Owner: vpusihwbpvbaxr
---
-
-ALTER TABLE ONLY public."recipes/ingredients"
-    ADD CONSTRAINT "recipes/ingredients_fk0" FOREIGN KEY ("recipeId") REFERENCES public.recipes(id);
-
-
---
--- Name: recipes/ingredients recipes/ingredients_fk1; Type: FK CONSTRAINT; Schema: public; Owner: vpusihwbpvbaxr
---
-
-ALTER TABLE ONLY public."recipes/ingredients"
-    ADD CONSTRAINT "recipes/ingredients_fk1" FOREIGN KEY ("ingredientId") REFERENCES public.ingredients(id);
+    ADD CONSTRAINT "recipes/categories_fk1" FOREIGN KEY ("categoryId") REFERENCES public.categories(id) ON DELETE CASCADE;
 
 
 --
@@ -938,7 +884,7 @@ ALTER TABLE ONLY public."recipes/ingredients"
 --
 
 ALTER TABLE ONLY public.recipes
-    ADD CONSTRAINT recipes_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id);
+    ADD CONSTRAINT recipes_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --
@@ -946,7 +892,7 @@ ALTER TABLE ONLY public.recipes
 --
 
 ALTER TABLE ONLY public.sessions
-    ADD CONSTRAINT sessions_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id);
+    ADD CONSTRAINT sessions_fk0 FOREIGN KEY ("userId") REFERENCES public.users(id) ON DELETE CASCADE;
 
 
 --
